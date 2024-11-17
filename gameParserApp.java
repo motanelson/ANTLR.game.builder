08abbad9a1f4aeebeb836ac5105b063832ec7d26
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-11-17 16:00:11 +0000

    Add files via upload

--- gameParserApp.java
+++ gameParserApp.java
@@ -4,9 +4,12 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class gameParserApp {
-    public static void main(String[] args) {
+
+        public static void main(String[] args) {
         boolean errors =false;
         String s="";
         Scanner scanner = new Scanner(System.in);
@@ -24,29 +27,46 @@ public class gameParserApp {
 
             try {
                 // Cria um CharStream a partir da entrada do usuário
+                
                 CharStream charStream = CharStreams.fromString(input);
 
                 // Inicializa o lexer e o parser com o CharStream
                 gameLexer lexer = new gameLexer(charStream);
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 gameParser parser = new gameParser(tokens);
+                
+                CustomErrorListener errorListener = new CustomErrorListener();
+                lexer.removeErrorListeners(); // Remove o ErrorListener padrão
+                lexer.addErrorListener(errorListener); // Adiciona o listener personalizado
+                parser.removeErrorListeners(); // Remove o ErrorListener padrão
+                parser.addErrorListener(errorListener); // Adiciona o listener personalizado
 
+            // Inicia o parsing (ajuste a regra inicial conforme sua gramática)
                 // Inicia o parser a partir da regra de entrada do arquivo gameParser
                 ParseTree tree = parser.program();  // Altere "program" para a regra de entrada correta
+                errors =false;
+                if (errorListener.hasErrors()) {
+                    System.out.println("Erros encontrados:");
+                    for (String error : errorListener.getErrors()) {
+                    errors =true;
 
+                }}
+                
+                
+                
                 // Exibe a árvore sintática
                 System.out.println("----------------------------------- ");
 
                 // Exibe os tokens da linha de entrada
                 System.out.println("");
                 tokens.fill();
-                errors =false;
+                
                 for (Token token : tokens.getTokens()) {
                     s=token.getText().toString();
                     if (s.indexOf("line")>-1){
                         System.out.printf("error\n");
                         errors=true;
-                        break;
+                        
                     }else{
                         if(!errors){
                             if (s.indexOf("CASTLE")>-1){
